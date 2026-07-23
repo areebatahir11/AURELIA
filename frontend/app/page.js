7093877b07f069cd08833448db3a7d7b6b774c35
areebatahir11 <areebatahir338@gmail.com>
2026-07-23 23:03:33 +0500

    feat(home): create initial landing page foundation

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,7 +1,11 @@
+import Hero from "@/features/home/Hero";
+import FeaturedCollection from "@/features/home/FeaturedCollection";
+
 export default function Home() {
   return (
-    <p className="text-5xl font-bold text-black bg-yellow-200 p-6 rounded-xl text-center">
-      It is Aurelia 
-    </p>
+    <>
+      <Hero />
+      <FeaturedCollection />
+    </>
   );
 }
